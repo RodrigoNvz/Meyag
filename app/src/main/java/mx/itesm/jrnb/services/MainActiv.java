61ea9dbb9a83e403de9235b28e9637464468c6d5
Rodrigo Nvz <a01377566@itesm.mx>
2018-02-11 13:36:28 -0600

    Initial commit

---
+++ app/src/main/java/mx/itesm/jrnb/services/MainActiv.java
@@ -0,0 +1,13 @@
+package mx.itesm.jrnb.services;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActiv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
